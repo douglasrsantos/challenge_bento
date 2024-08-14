f48d7a6247be03572eaed93a290593c586538662
Douglas Santos <douglasrosasantos@gmail.com>
2024-08-14 20:40:14 -0300

    feat: configured Materialapp and inserted the words in English

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'package:challenge_bento/app/core/routes/routes.dart';
 import 'package:challenge_bento/app/core/ui/ui.dart';
@@ -10,6 +11,16 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'Challenge Bento',
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'),
+        Locale('pt'),
+      ],
+      locale: const Locale('en'),
       debugShowCheckedModeBanner: false,
       routerDelegate: routes.routerDelegate,
       routeInformationParser: routes.routeInformationParser,
