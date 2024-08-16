9462eec17c89f0b4a95ab65a584d8159409506a0
Douglas Santos <douglasrosasantos@gmail.com>
2024-08-16 08:28:02 -0300

    test user service impl: throws error no data  if json file is empty

--- test/app/core/services/user/user_service_impl_test.dart
+++ test/app/core/services/user/user_service_impl_test.dart
@@ -1,8 +1,9 @@
-import 'package:challenge_bento/app/core/models/models.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:challenge_bento/app/core/error/error.dart';
+import 'package:challenge_bento/app/core/models/models.dart';
 import 'package:challenge_bento/app/core/services/services.dart';
 
 import 'user_service_impl_test.mocks.dart';
@@ -39,5 +40,12 @@ void main() {
     final user = await userService.getUser();
 
     expect(user, null);
+  });
+
+  test('should return error no data if json file is empty', () async {
+    when(userService.getUser()).thenThrow(RequestError.noData);
+
+    expect(
+        () async => await userService.getUser(), throwsA(RequestError.noData));
   });
 }
